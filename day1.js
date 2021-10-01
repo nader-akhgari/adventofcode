14c70bce17cb61e85d9f4f8e44a52af875a196fa
nader <nader.akhgari@ft.com>
2021-10-01 18:42:54 +0100

    day 1 initial solution

--- day1.js
+++ day1.js
@@ -1,5 +1,15 @@
 function day1(coins) {
-  return 514579;
+  for(let i=0; i<coins.length; i++){
+   for(let j=1; j<coins.length; j++){
+     if(coins[i] + coins[j] === 2020){
+       console.log('coin',coins[i] , coins[j])
+       return[coins[i], coins[j]]
+     }
+     
+   }
+  }
+  
+  return [];
 }
 
 module.exports = { day1 };
--- day1.test.js
+++ day1.test.js
@@ -3,6 +3,10 @@ const { day1 } = require('./day1');
 describe('day1', () => {
   it('finds multiple of values that sum to 2020', () => {
     const values = [1721, 979, 366, 299, 675, 1456];
-    expect(day1(values)).toBe(514579);
+    expect(day1(values)).toEqual([1721, 299]);
+  })
+  it('finds multiple of values that sum to 2020', () => {
+    const values = [1010, 1721, 979, 366, 299, 675, 1456];
+    expect(day1(values)).toEqual([1721, 299]);
   })
 })
