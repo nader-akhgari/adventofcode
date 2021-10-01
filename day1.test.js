58f6f74493c9fabc5919658ddb4e38121cd88af4
Jimijos <jimingeorge@gmail.com>
2021-10-01 19:18:29 +0100

    the day1 solution

--- day1.test.js
+++ day1.test.js
@@ -5,8 +5,18 @@ describe("day1", () => {
     const values = [1721, 979, 366, 299, 675, 1456];
     expect(day1(values)).toEqual([1721, 299]);
   });
-  it("finds multiple of values that sum to 2020", () => {
+  it("returns the results with different numbers", () => {
     const values = [1010, 1721, 979, 366, 299, 675, 1456];
     expect(day1(values)).toEqual([1721, 299]);
+  });
+
+  it("return an array of numbers", () => {
+    const values = `1760
+    1888
+    1639
+    1282
+    1633
+    1619`;
+    expect(day1Data(values)).toEqual([1760, 1888, 1639, 1282, 1633, 1619]);
   });
 });
