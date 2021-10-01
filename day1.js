0e0477990e3855be2728a4cef58f04c781dc4fd5
Jimijos <jimingeorge@gmail.com>
2021-10-01 19:15:01 +0100

    the day1 solution

--- day1.js
+++ day1.js
@@ -1,15 +1,25 @@
+// const fs = require("fs");
+const input = require("./day1-input");
+
 function day1(coins) {
-  for(let i=0; i<coins.length; i++){
-   for(let j=1; j<coins.length; j++){
-     if(coins[i] + coins[j] === 2020){
-       console.log('coin',coins[i] , coins[j])
-       return[coins[i], coins[j]]
-     }
-     
-   }
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 1; j < coins.length; j++) {
+      if (coins[i] + coins[j] === 2020) {
+        console.log("coin", coins[i], coins[j]);
+        return [coins[i], coins[j]];
+      }
+    }
   }
-  
+
   return [];
 }
 
-module.exports = { day1 };
+module.exports = { day1, day1Data: convertData };
+
+function convertData(data) {
+  return data.split("\n").map((str) => +str);
+}
+
+const day1Data = convertData(input);
+const day1Pair = day1(day1Data);
+console.log("daya1 part 1 result:", day1Pair[0] * day1Pair[1]);
--- day1.test.js
+++ day1.test.js
@@ -1,12 +1,12 @@
-const { day1 } = require('./day1');
+const { day1, day1Data } = require("./day1");
 
-describe('day1', () => {
-  it('finds multiple of values that sum to 2020', () => {
+describe("day1", () => {
+  it("finds multiple of values that sum to 2020", () => {
     const values = [1721, 979, 366, 299, 675, 1456];
     expect(day1(values)).toEqual([1721, 299]);
-  })
-  it('finds multiple of values that sum to 2020', () => {
+  });
+  it("finds multiple of values that sum to 2020", () => {
     const values = [1010, 1721, 979, 366, 299, 675, 1456];
     expect(day1(values)).toEqual([1721, 299]);
-  })
-})
+  });
+});
