9d4807ef4ee96695e3324b5bed81d92fed4ab120
nader <nader.akhgari@ft.com>
2021-10-03 13:00:14 +0100

    change the second loop to cover all posibilities

--- day1.js
+++ day1.js
@@ -3,7 +3,7 @@ const input = require("./day1-input");
 
 function day1(coins) {
   for (let i = 0; i < coins.length; i++) {
-    for (let j = 1; j < coins.length; j++) {
+    for (let j = i+1; j < coins.length; j++) {
       if (coins[i] + coins[j] === 2020) {
         console.log("coin", coins[i], coins[j]);
         return [coins[i], coins[j]];
--- day1.test.js
+++ day1.test.js
@@ -6,7 +6,7 @@ describe("day1", () => {
     expect(day1(values)).toEqual([1721, 299]);
   });
   it("returns the results with different numbers", () => {
-    const values = [1010, 1721, 979, 366, 299, 675, 1456];
+    const values = [979, 366, 1010, 1721, 299, 675, 1456];
     expect(day1(values)).toEqual([1721, 299]);
   });
 
