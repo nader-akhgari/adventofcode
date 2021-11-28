aaf12d80c85be7dbafcc80c5e89f06d734f93484
nader <nader.akhgari@ft.com>
2021-11-28 21:11:25 +0000

    change the test description

--- day2/day2-part2.test.js
+++ day2/day2-part2.test.js
@@ -1,6 +1,6 @@
 const day2Part2 = require('./day2-part2.js')
 describe('day2 part2',()=>{
-    it('should return true',()=>{
+    it('should return 2',()=>{
         const passData = [
             { char: "l", min: "15", max: "16", password: "klfbblslvjclmlnqklvg" },
             { char: "h", min: "6", max: "13", password: "pghjchdxhnjhjd" },
