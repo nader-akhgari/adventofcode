79d11307896ba8c8a54eeb39d1c5648270efd02b
nader <nader.akhgari@ft.com>
2022-02-06 12:53:28 +0000

    refactor day5.js

--- day5/day5.js
+++ day5/day5.js
@@ -1,45 +1,34 @@
-// const dataInput = ['BFFFBBFRRR', 'FFFBBBFRRR', 'BBFFBBFRLL']
 const input = require('./input')
 const dataInput = input.split('\n')
+
 const sanityCheck = (data1) => {
   const seatNums = data1.map(data => colculateSeat(data))
   return Math.max(...seatNums)
-  
 };
 
 const colculateSeat = (data)=>{
-
   const formattedRows = data.split("").slice(0, 7);
   const formattedColumn = data.split("").slice(7, 10);
-  let rows = [];
-  const cols = [];
-  for (let i = 0; i < 128; i++) {
-    rows.push(i);
-  }
-
-  for (let i = 0, j = 64; i < 7; i++, j = j / 2) {
-    if (formattedRows[i] === "F") {
-      rows.splice(j, j);
-    } else if (formattedRows[i] === "B") {
-      rows.splice(0, j);
-    }
-  }
+  let rows = buildSeats(64, 7, 'F', 'B', formattedRows)
+  const cols = buildSeats(4, 3, 'L', 'R',formattedColumn);
   let row = rows[0];
+  const column = cols[0];
+  return row * 8 + column;
+}
 
-  for (let i = 0; i < 8; i++) {
+const buildSeats = (row, col, low, high, allRows)=>{
+  const cols =[]
+  for (let i = 0; i < (row * 2); i++) {
     cols.push(i);
   }
-
-  for (let i = 0, j = 4; i < 3; i++, j = j / 2) {
-    if (formattedColumn[i] === "L") {
+  for (let i = 0, j = row; i < col; i++, j = j / 2) {
+    if (allRows[i] === low) {
       cols.splice(j, j);
-    } else if (formattedColumn[i] === "R") {
+    } else if (allRows[i] === high) {
       cols.splice(0, j);
     }
   }
-  const column = cols[0];
-  return row * 8 + column;
-
+  return cols
 }
 
 console.log(sanityCheck(dataInput));
